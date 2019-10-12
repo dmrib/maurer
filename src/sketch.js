f7f7d53a5cb4b8b96d2baff444a11da4c220d703
dmrib <dmrib.cs@gmail.com>
2019-10-12 12:46:11 -0300

    Add p5.js boilerplate code

---
+++ src/sketch.js
@@ -0,0 +1,21 @@
+/*
+* p5.js setup function.
+*/
+function setup()
+{
+  // create canvas
+  createCanvas(500, 500);
+
+  // set frame rate
+  frameRate(10);
+}
+
+
+/*
+* p5.js drawing callback.
+*/
+function draw()
+{
+  // clear background
+  background(181, 191, 200);
+}
